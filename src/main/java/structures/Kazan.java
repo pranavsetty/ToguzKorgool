acff9def68e8770d66215b05b3ea1b7fe3d2bb04
Thomas <thomas.pappas@kcl.ac.uk>
2018-12-03 20:05:48 +0000

    Add accessor and mutator methods to Hole and Kazan Class

--- src/main/java/structures/Kazan.java
+++ src/main/java/structures/Kazan.java
@@ -4,11 +4,23 @@ public class Kazan {
 
     int korgols;
 
+    //Constructor which creates a Kazan with "input Korgols"
     public Kazan(int input){
         korgols = input;
     }
 
+    //Accessor method for number of korgols in Kazan
     public int GetKorgols(){return korgols;}
 
+    //Mutator method which adds one korgol to the Kazan
+    public void addKorgol(){korgols++;}
+
+    //Mutator method which adds an indefinite number of korgols to the Kazan
+    public void addKorgols(int newKorgs){
+      korgols+=number;
+    }
+
+    public void clearKazan(){korgols = 0;}
+    
 
 }
