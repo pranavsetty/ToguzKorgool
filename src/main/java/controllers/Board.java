c7ee831d20aa283b482f4c1e5ba8a7001b09742b
K1764082 <k1764082@kcl.ac.uk>
2018-12-07 13:03:20 +0000

    Implement board configuration

--- src/main/java/controllers/Board.java
+++ src/main/java/controllers/Board.java
@@ -1,9 +1,13 @@
 package controllers;
 
-import structures.*;
+import structures.Configuration;
+import structures.Hole;
+import structures.Kazan;
+
+import java.util.ArrayList;
 
 public class Board {
-    private arrayList<Hole> board = new arrayList(18);
+    private ArrayList<Hole> board = new ArrayList<Hole>(18);
     private Kazan playerOneKazan;
     private Kazan playerTwoKazan;
 
@@ -13,20 +17,29 @@ public class Board {
         }
     }
 
+    Board(Configuration config) {
+        playerOneKazan = config.GetBlackKazan();
+        playerTwoKazan = config.GetWhiteKazan();
+        for (int i = 0; i<18; i++) {
+            Hole holeToAdd = config.GetHoles().get(i);
+            board.add(new Hole(holeToAdd.getKorgols()));
+        }
+    }
+
     public void move(Hole targetHole) {
         int ballsToMove = targetHole.getKorgols();
         int targetHoleIndex = board.indexOf(targetHole);
-        targetHole.clear()
+        targetHole.clear();
         Hole finalHole = board.get((targetHoleIndex + ballsToMove - 1) % 18);
         for (int i = targetHoleIndex; i < (targetHoleIndex + ballsToMove); i++) {
             board.get(i % 18).addKorgol();
         }
         if (board.indexOf(targetHole) < 9 && board.indexOf(finalHole) >= 9 && finalHole.isEven()) {
-            playerOneKazan.addKorgols(finalHole.getKorgols);
+            playerOneKazan.addKorgols(finalHole.getKorgols());
             finalHole.clear();
         }
         else if (board.indexOf(targetHole) >= 9 && board.indexOf(finalHole) < 9 && finalHole.isEven()) {
-            playerOneKazan.addKorgols(finalHole.getKorgols);
+            playerOneKazan.addKorgols(finalHole.getKorgols());
             finalHole.clear();
         }
         else {}
